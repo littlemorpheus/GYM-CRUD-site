6a734b42f049c5076057967f6629c584d8256231
lilmorpheus <Escott97@hotmail.co.uk>
2022-03-15 18:58:20 +0000

    Forgot Charger

--- routing/routes/index.route.js
+++ routing/routes/index.route.js
@@ -6,11 +6,11 @@ const CTRL_WO_DIARY = require('../controllers/workout-diary.controller');
 const CTRL_WO_CHILD = require('../controllers/workout-child.controller');
 const CTRL_EXERCISE = require('../controllers/exercise.controller');
 const ctrl_media = require('../controllers/media.controller');
+const { verifyAuth } = require('../auth');
+
 const ctrl_wo = new CTRL_WO;
 const ctrl_wo_diary = new CTRL_WO_DIARY;
 const ctrl_wo_child = new CTRL_WO_CHILD;
-//I think even as I change this best to keep this as Workout Child
-//FrontEnd can change name if need
 const ctrl_exercise = new CTRL_EXERCISE;
 
 /*    GET Requests    */
@@ -28,17 +28,17 @@ router.get('/workouts', ctrl_wo.getAll)
 router.get('/workout-diaries', ctrl_wo_diary.getAll)
 
 /*    POST Requests    */
-router.post('/exercise', ctrl_exercise.add);
-router.post('/workout-child', ctrl_wo_child.add);
-router.post('/workout-diary', ctrl_wo_diary.add);
-router.post('/workout', ctrl_wo.add)
-router.post('/img', ctrl_media.upload.single('file'), ctrl_media.addImage)
+router.post('/exercise', verifyAuth, ctrl_exercise.add);
+router.post('/workout-child', verifyAuth, ctrl_wo_child.add);
+router.post('/workout-diary', verifyAuth, ctrl_wo_diary.add);
+router.post('/workout', verifyAuth, ctrl_wo.add)
+//router.post('/img', ctrl_media.upload.single('file'), ctrl_media.addImage)
 
 
 /*    DELETE Requests    */
-router.delete('/exercises/:id', ctrl_exercise.del);
-router.delete('/workout-children/:id', ctrl_wo_child.del);
-router.post('/img/:id', ctrl_media.delImage)
-router.delete('/workouts/:id', ctrl_wo.del)
+router.delete('/exercises/: id', verifyAuth, ctrl_exercise.del);
+router.delete('/workout-children/:id', verifyAuth, ctrl_wo_child.del);
+//router.post('/img/:id', ctrl_media.delImage)
+router.delete('/workouts/:id', verifyAuth, ctrl_wo.del)
 
 module.exports = router;
